ac44c8e72fa953baa4cc3a8cdeeab99e732aee81
Yonghyu Ban <yonghyu@empo.im>
2020-12-08 17:48:01 +0900

    Partial implementation of ipTIME login function

--- src/driver/efm/lib.ts
+++ src/driver/efm/lib.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
+import FormData from 'form-data';
 import { ControllerFactory as GenericControllerFactory } from '../generic/lib';
-import { WLANConfigurator as EFMWLANConfigurator } from './wifi';
+import { WLANConfigurator as EFMWLANConfigurator } from './wlan';
 
 export class ControllerFactory extends GenericControllerFactory {
     protected api: AxiosInstance;
@@ -13,6 +14,19 @@ export class ControllerFactory extends GenericControllerFactory {
                 Referer: deviceaddress, // CSRF bypass
             },
         });
+    }
+
+    public async authenticate(credential: {id: string, pass: string}) {
+        const form = new FormData();
+        form.append('init_status', 1);
+        form.append('captcha_on', 0); // Cannot support captcha_on, unless we train the neural net.
+        form.append('username', credential.id);
+        form.append('passwd', credential.pass);
+        const res = await this.api.post('/sess-bin/login_handler.cgi', form, {
+            headers: form.getHeaders(),
+        });
+
+        // TODO: Extract efm_session_id from response javascript
     }
 
     public getWLANConfigurator(): EFMWLANConfigurator {
--- src/driver/generic/lib.ts
+++ src/driver/generic/lib.ts
@@ -1,6 +1,13 @@
-export class ControllerFactory {
+export abstract class ControllerFactory {
     protected deviceaddress: string;
+    protected credential: any;
     constructor(deviceaddress: string) {
         this.deviceaddress = deviceaddress;
     }
+
+    protected updateCredential(cred: any) {
+        this.credential = cred;
+    }
+
+    abstract authenticate(credential: any): Promise<void>;
 }
