27c52347dae2c327e8861a93edee271590d111ed
Yonghyu Ban <yonghyu@empo.im>
2020-12-10 16:34:06 +0900

    [impl] EFMWlanConfigurator.getDeviceCfg and gdtIFaceCfg

--- src/driver/efm/wlan.ts
+++ src/driver/efm/wlan.ts
@@ -5,6 +5,50 @@ import { WLANIFaceCfg } from '../efm/WLANIFaceCfg';
 import qs from 'qs';
 import { JSDOM } from 'jsdom';
 
+// Internal types for ipTIME
+interface DeviceCfg {
+    wlmode: string,
+    wlmodetxt: string,
+    ctlchannel: string,
+    cntchannel: string,
+    chandisable: string,
+    dfswarning: string,
+    run: string,
+    mbsspolicy: string,
+    txpower: string,
+    beacon: string,
+    rts: string,
+    wpsmode: string,
+    wmm: string,
+    dynchannel: string,
+    dcsperiodhour: string,
+    wirelessmode: string,
+    country: string,
+    channelwidth: string,
+    txbfmode: string,
+    vht24g: string,
+    mimoant: string,
+    realchannelwidth: string,
+}
+
+interface IFaceCfg {
+    run: string,
+    wmm: string,
+    ssid: string,
+    txrate: string,
+    rxrate: string,
+    qosenable: string,
+    useenterprise: string,
+    radiusserver: string,
+    radiussecret: string,
+    radiusport: string,
+    enterpriselist: string,
+    personallist: string,
+    wpapsk: string,
+    wepkey: string,
+    broadcast: string,
+}
+
 export class WLANConfigurator extends GenericWLANConfigurator {
     protected api: AxiosInstance;
     constructor(axios: AxiosInstance) {
@@ -29,15 +73,26 @@ export class WLANConfigurator extends GenericWLANConfigurator {
 
         const dom = new JSDOM(res.data);
         const fields = dom.window.document.body.getElementsByTagName('input');
-        const map = {} as any;
+        const devcfg = {} as DeviceCfg;
         for (let i = 0; i < fields.length; i++) {
             const field = fields[i];
-            map[field.name] = field.value;
+            (devcfg as any)[field.name] = field.value; // Override... for now.
         }
 
-        console.log(map)
+        const wlanDevCfg = new WLANDevConfiguration();
+        wlanDevCfg.disabled = devcfg.run === '1';
+        wlanDevCfg.channel = parseInt(devcfg.cntchannel, 10);
+        wlanDevCfg.hwmode = devcfg.wlmode === '1' ? 'a' : 'g';
+        wlanDevCfg.htmode = 'HT20'; // Does not affect here
+        wlanDevCfg.chanbw = devcfg.channelwidth as any;
+        wlanDevCfg.txpower = parseInt(devcfg.txpower, 10);
+        wlanDevCfg.diversity = parseInt(devcfg.mimoant, 10) > 1;
+        wlanDevCfg.country = 'KR' // Hardcode it for now.. TODO: FIXME! Write adapter for it!
+        wlanDevCfg.country_ie = true; // There is no option for it.
+        wlanDevCfg.beacon_int = parseInt(devcfg.beacon, 10);
+        wlanDevCfg.require_mode = 'none'; // Unsupported.
 
-        return new WLANDevConfiguration();
+        return wlanDevCfg;
     }
 
     async setDeviceCfg(devname: string, cfg: WLANDevConfiguration) {
@@ -50,7 +105,7 @@ export class WLANConfigurator extends GenericWLANConfigurator {
             smenu: 'hiddenwlsetup',
             wlmode: (devname === 'wlan5g') ? 1 : 0, // Crude but it will work
             action: 'changebssid',
-            sidx: 1,
+            sidx: parseInt(ifname, 10),
         }
 
         const res = await this.api.post('/sess-bin/timepro.cgi', qs.stringify(params), {
@@ -61,15 +116,27 @@ export class WLANConfigurator extends GenericWLANConfigurator {
 
         const dom = new JSDOM(res.data);
         const fields = dom.window.document.body.getElementsByTagName('input');
-        const map = {} as any;
+        const ifacecfg = {} as IFaceCfg;
         for (let i = 0; i < fields.length; i++) {
             const field = fields[i];
-            map[field.name] = field.value;
+            (ifacecfg as any)[field.name] = field.value;
         }
 
-        console.log(map)
-        // TODO: Implement meee...
-        return new WLANIFaceCfg();
+        const wlanIfaceCfg = new WLANIFaceCfg();
+        
+        wlanIfaceCfg.mode = 'ap'; // TODO: Support STA mode when possible.
+        wlanIfaceCfg.disabled = ifacecfg.run === '0';
+        wlanIfaceCfg.ssid = ifacecfg.ssid;
+        wlanIfaceCfg.bssid = 'default'; // ipTIME does not support bssid spoofing
+        wlanIfaceCfg.mesh_id = 'none'; // TODO: Research EasyMesh
+        wlanIfaceCfg.hidden = ifacecfg.broadcast === '0';
+        wlanIfaceCfg.isolate = false; // TODO: Implement this
+        wlanIfaceCfg.doth = true; // There is no feature to disable it
+        wlanIfaceCfg.wmm = ifacecfg.wmm === '1';
+        wlanIfaceCfg.encryption = ifacecfg.personallist; // TODO: Translation layer between OpenWRT notation and ipTIME notation
+        wlanIfaceCfg.key = ifacecfg.wpapsk; // TODO: Support for other mechanisms
+
+        return wlanIfaceCfg;
     }
 
     async setIFaceCfg(devname: string, ifname: string, cfg: WLANIFaceCfg) {
