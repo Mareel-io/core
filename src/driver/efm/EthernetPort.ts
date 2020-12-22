e4f831155c68f63ffa986fba96e0fda8b2204ce1
perillamint <perillamint@quendi.moe>
2020-12-22 13:36:12 +0900

    Document some new methods

--- src/driver/efm/EthernetPort.ts
+++ src/driver/efm/EthernetPort.ts
@@ -1,6 +1,9 @@
 import { EthernetPort as GenericEthernetPort } from '../generic/EthernetPort';
 
 export class EthernetPort extends GenericEthernetPort {
+    /**
+     * EFM-specific taglabel generator
+     */
     public get tagLabel(): string {
         if (this.tag === 1) {
             return 'WAN';
