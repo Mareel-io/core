41d301e8b90a87b303782080de96610e3342f581
Yonghyu Ban <yonghyu@empo.im>
2021-03-22 17:09:44 +0900

    (WIP) Partial implementation of SNMP walker

--- src/util/snmp/walker.ts
+++ src/util/snmp/walker.ts
@@ -1,3 +1,30 @@
+import snmp from 'net-snmp';
+import { MIBLoader } from './mibloader';
+
+interface SNMPConfig {
+    id: string,
+    authProtocol: undefined | null | 'sha' | 'md5',
+    authKey: undefined | null | string,
+    privacyProtocol: undefined | null | 'des' | 'aes',
+    privatcyKey: undefined | null | string,
+};
+
 export class SNMPWalker {
-    //
+    private cfg: SNMPConfig;
+    private mibLoader: MIBLoader;
+    private snmpSession: any;
+    private target: string;
+
+    constructor(target: string, cfg: SNMPConfig, mibLoader: MIBLoader) {
+        this.cfg = cfg;
+        this.mibLoader = mibLoader;
+        this.target = target;
+    }
+
+    connect() {
+        // TODO: Find way to translate SNMPconfig to API
+        snmp.createV3Session(this.target, {
+            name: this.cfg.id,
+        });
+    }
 }
