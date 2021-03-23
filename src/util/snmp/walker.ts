75729acd5db23b680b49ae2a148896fbb59c5b4a
Yonghyu Ban <yonghyu@empo.im>
2021-03-23 18:07:10 +0900

    SNMP walker implementation

--- src/util/snmp/walker.ts
+++ src/util/snmp/walker.ts
@@ -21,10 +21,59 @@ export class SNMPWalker {
         this.target = target;
     }
 
-    connect() {
-        // TODO: Find way to translate SNMPconfig to API
-        snmp.createV3Session(this.target, {
+    public connect() {
+        // TODO: Find way to translate SNMPconfig to createV3Session API
+        let level = snmp.SecurityLevel.noAuthNoPriv;
+        let authProtocol = null;
+        let privProtocol = null;
+
+        if (this.cfg.authProtocol != null) {
+            if (this.cfg.privacyProtocol != null) {
+                level = snmp.SecurityLevel.authPriv;
+            } else {
+                level = snmp.SecurityLevel.authNoPriv;
+            }
+        }
+
+        switch(this.cfg.authProtocol) {
+            case 'sha':
+                authProtocol = snmp.AuthProtocols.sha;
+                break;
+        }
+
+        switch(this.cfg.privacyProtocol) {
+            case 'des':
+                privProtocol = snmp.PrivProtocols.des;
+                break;
+        }
+
+        this.snmpSession = snmp.createV3Session(this.target, {
             name: this.cfg.id,
+            level: level,
+            authProtocol: authProtocol,
+            authKey: this.cfg.authKey,
+            privProtocol: privProtocol,
+            privKey: this.cfg.privatcyKey,
+        });
+    }
+
+    public async walk(oid: string, depth: number): Promise<{oid: string, value: any}[]> {
+        const valuePairs: {oid: string, value: any}[] = [];
+        return new Promise((ful, rej) => {
+            this.snmpSession.walk(oid, depth, (varbinds: {oid: string, value: any}[]) => {
+                varbinds.forEach((elem) => {
+                    valuePairs.push({
+                        oid: elem.oid,
+                        value: elem.value,
+                    });
+                });
+            }, (e: Error) => {
+                if (e != null) {
+                    rej(e);
+                } else {
+                    ful(valuePairs);
+                }
+            });
         });
     }
 }
