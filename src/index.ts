d826e1660ec134baa2c074242c95d9072d56207e
Yonghyu Ban <yonghyu@empo.im>
2021-03-26 14:57:39 +0900

    Export Cisco driver class

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export class Mareel {
 }
 
 export { ControllerFactory as EFMControllerFactory} from './driver/efm/lib';
+export { ControllerFactory as CiscoControllerFactory} from './driver/cisco/lib';
 export { ControllerFactory as GenericControllerFactory} from './driver/generic/lib';
 
 // Test export
