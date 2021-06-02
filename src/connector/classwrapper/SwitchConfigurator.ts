6c417354206d72b139a89061ed717155ba315ea4
Yonghyu Ban <yonghyu@empo.im>
2021-06-02 13:26:15 +0900

    Convert some classes into serializable object and vice versa

--- src/connector/classwrapper/SwitchConfigurator.ts
+++ src/connector/classwrapper/SwitchConfigurator.ts
@@ -1,6 +1,10 @@
-import { EthernetPort } from '../../driver/generic/EthernetPort';
+import { EthernetPort as CiscoEthernetPort } from '../../driver/cisco/EthernetPort';
+import { EthernetPort as EFMEthernetPort } from '../../driver/efm/EthernetPort';
+import { EthernetPort as GenericEthernetPort } from '../../driver/generic/EthernetPort';
+import { VLAN as GenericVLAN } from '../../driver/generic/VLAN';
+import { VLAN as CiscoVLAN } from '../../driver/cisco/VLAN';
+import { VLAN as EFMVLAN } from '../../driver/efm/VLAN';
 import { SwitchConfigurator as GenericSwitchConfigurator } from '../../driver/generic/SwitchConfigurator';
-import { VLAN } from '../../driver/generic/VLAN';
 import { RPCProvider } from '../../util/jsonrpcv2';
 
 export class RPCSwitchConfigurator extends GenericSwitchConfigurator {
@@ -14,58 +18,81 @@ export class RPCSwitchConfigurator extends GenericSwitchConfigurator {
         this.targetId = targetId;
     }
 
-    public async getSwitchPorts(): Promise<EthernetPort[]> {
+    public async getSwitchPorts(): Promise<GenericEthernetPort[]> {
         // TODO: Unwrap EthernetPort from serialized something
-        return await this.rpc.remoteCall({
+        const ret: {[key: string]: any}[] = await this.rpc.remoteCall({
             jsonrpc: '2.0',
             target: this.targetId,
             class: 'SwitchConfigurator',
             method: 'getSwitchPorts',
             params: [],
         });
+
+        return ret.map((stat) => {
+            // TODO: FIXME: Depends on the platform, instantiate different EthernetPort obj
+            const ethPort = new CiscoEthernetPort();
+
+            ethPort.restore(stat);
+            return ethPort;
+        });
     }
 
-    public async setSwitchPort(port: EthernetPort, portIdx: number): Promise<void> {
+    public async setSwitchPort(port: GenericEthernetPort, portIdx: number): Promise<void> {
         // TODO: Wrap port into serializable
+        const serializedPort = port.serialize()
         return await this.rpc.remoteCall({
             jsonrpc: '2.0',
             target: this.targetId,
             class: 'SwitchConfigurator',
             method: 'setSwitchPort',
-            params: [port, portIdx],
+            params: [serializedPort, portIdx],
         });
     }
 
-    public async getAllVLAN(): Promise<VLAN[]> {
+    public async getAllVLAN(): Promise<GenericVLAN[]> {
         // TODO: Unwrap VLAN[] from serialized result
-        return await this.rpc.remoteCall({
+        const ret: {[key: string]: any}[] = await this.rpc.remoteCall({
             jsonrpc: '2.0',
             target: this.targetId,
             class: 'SwitchConfigurator',
             method: 'getAllVLAN',
             params: [],
         });
+
+        return ret.map((stat) => {
+            // TODO: FIXME: Depends on the platform, instantiate different VLAN obj
+            const vlan = new CiscoVLAN('802.1q');
+            vlan.restore(stat);
+
+            return vlan;
+        });
     }
 
-    public async getVLAN(vid: number): Promise<VLAN | null> {
-        // TODO: Unwrap VLAN into serializable something
-        return await this.rpc.remoteCall({
+    public async getVLAN(vid: number): Promise<GenericVLAN | null> {
+        const ret: {[key: string]: any} | null = await this.rpc.remoteCall({
             jsonrpc: '2.0',
             target: this.targetId,
             class: 'SwitchConfigurator',
             method: 'getVLAN',
             params: [vid],
         });
+
+        if (ret != null) {
+            const vlan = new CiscoVLAN('802.1q');
+            vlan.restore(ret);
+            return vlan;
+        } else {
+            return null;
+        }
     }
 
-    public async setVLAN(vlan: VLAN): Promise<void> {
-        // TODO: Wrap VLAN into serializable something
+    public async setVLAN(vlan: GenericVLAN): Promise<void> {
         return await this.rpc.remoteCall({
             jsonrpc: '2.0',
             target: this.targetId,
             class: 'SwitchConfigurator',
             method: 'getVLAN',
-            params: [vlan],
+            params: [vlan.serialize()],
         });
     }
 }
