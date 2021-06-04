5bc4157fb752ff0715f450866601c3ded54b229d
Yonghyu Ban <yonghyu@empo.im>
2021-06-04 12:31:44 +0900

    Implement RPCControllerFactory

--- src/connector/classwrapper/lib.ts
+++ src/connector/classwrapper/lib.ts
@@ -1,21 +1,28 @@
 import WebSocket from 'ws';
 
 import { GenericControllerFactory } from "../..";
+import { RPCProvider } from '../jsonrpcv2';
 import { RPCSwitchConfigurator } from "./SwitchConfigurator";
 
 export class RPCControllerFactory extends GenericControllerFactory {
     private ws: WebSocket;
+    private rpc: RPCProvider;
 
     constructor(ws: WebSocket) {
         super('');
         this.ws = ws;
+        const stream = WebSocket.createWebSocketStream(this.ws, {encoding: 'utf-8'});
+        this.rpc = new RPCProvider(stream);
     }
 
     public async authenticate(): Promise<void> {
         console.warn("You do not have to authenticate here");
     }
 
-    getSwitchConfigurator() {
-        // TODO: Implement me.
+    getSwitchConfigurator(): RPCSwitchConfigurator {
+        // TODO: FIXME: Implement target getter system rpc method
+        const targetId = 'f265a9b2-13cd-43a7-85d2-2f6ac24d0963';
+        const switchConfigurator = new RPCSwitchConfigurator(this.rpc, targetId);
+        return switchConfigurator;
     }
 }
