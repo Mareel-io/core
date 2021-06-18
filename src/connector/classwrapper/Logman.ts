05e9bba1017b1ac12fb445ce8c468572ec273d8b
Yonghyu Ban <yonghyu@empo.im>
2021-06-18 11:48:18 +0900

    Logman RPC sender implementation

---
+++ src/connector/classwrapper/Logman.ts
@@ -0,0 +1,30 @@
+import { LogEntry, Logman as GenericLogman } from '../../driver/generic/Logman';
+import { RPCProvider } from '../jsonrpcv2';
+
+export class RPCSwitchConfigurator extends GenericLogman {
+    private rpc: RPCProvider;
+    private targetId: string;
+
+    constructor(rpc: RPCProvider, targetId: string) {
+        super();
+
+        this.rpc = rpc;
+        this.targetId = targetId;
+    }
+
+    public async queryLog(source: string, from: Date | undefined, to: Date | undefined): Promise<LogEntry[]> {
+        const result: any[] = await this.rpc.remoteCall({
+            jsonrpc: '2.0',
+            target: this.targetId,
+            class: 'Logman',
+            method: 'queryLog',
+            params: [source, from, to],
+        });
+
+        return result.map((ent): LogEntry => {
+            const ln = new LogEntry(new Date(), '');
+            ln.restore(ent);
+            return ln;
+        });
+    }
+}
