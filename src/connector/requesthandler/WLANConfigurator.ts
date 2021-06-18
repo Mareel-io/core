dbcdfa26f244abeb8bf0a2e9ab635544f6d130df
Yonghyu Ban <yonghyu@empo.im>
2021-06-18 15:19:29 +0900

    Initial implementation of EFM client wrapper

--- src/connector/requesthandler/WLANConfigurator.ts
+++ src/connector/requesthandler/WLANConfigurator.ts
@@ -1,40 +1,75 @@
 import { WLANConfigurator as EFMWlanConfigurator} from "../../driver/efm/wlan";
+import { WLANIFaceCfg as EFMWLANIFaceCfg } from "../../driver/efm/WLANIFaceCfg";
 import { WLANConfigurator as GenericWLANConfigurator } from "../../driver/generic/wlan";
+import { WLANDevConfiguration, WLANDevConfigurationState } from "../../driver/generic/WLANDevConfiguration";
+import { WLANIFaceCfg as GenericWLANIFaceCfg, WLANIFaceCfgState } from "../../driver/generic/WLANIFaceCfg";
 import { RPCReturnType, RPCv2Request } from "../jsonrpcv2";
 import { RPCMethodTable, RPCRequestHandler } from "./RPCRequestHandler";
 
 export class WLANConfiguratorReqHandler extends RPCRequestHandler {
     private wlanConfigurator: GenericWLANConfigurator | EFMWlanConfigurator; // TODO: Add Dummy
     protected rpcMethodTable: RPCMethodTable = {
-        getDeviceList: async() => {
-            //
+        getDeviceList: async(): Promise<string[]> => {
+            return await this.wlanConfigurator.getDeviceList();
         },
-        getDeviceCfg: async() => {
-            //
+        getDeviceCfg: async(devname: string): Promise<WLANDevConfigurationState> => {
+            const deviceCfg = await this.wlanConfigurator.getDeviceCfg(devname);
+            return deviceCfg.serialize();
         },
-        setDeviceCfg: async() => {
-            //
+        setDeviceCfg: async(devname: string, cfgState: WLANDevConfigurationState): Promise<void> => {
+            const cfg = new WLANDevConfiguration();
+            cfg.restore(cfgState);
+            await this.wlanConfigurator.setDeviceCfg(devname, cfg);
         },
-        getIFaceCfg: async() => {
-            //
+        getIFaceCfg: async(devname: string, ifname: string): Promise<WLANIFaceCfgState> => {
+            const cfg = await this.wlanConfigurator.getIFaceCfg(devname, ifname);
+            return cfg.serialize();
         },
-        setIFaceCfg: async() => {
-            //
+        setIFaceCfg: async(devname: string, ifname: string, cfgStat: WLANIFaceCfgState): Promise<void> => {
+            if (this.wlanConfigurator instanceof EFMWlanConfigurator) {
+                const cfg = new EFMWLANIFaceCfg();
+                cfg.restore(cfgStat);
+                await this.wlanConfigurator.setIFaceCfg(devname, ifname, cfg);
+            } else {
+                const cfg = new GenericWLANIFaceCfg();
+                cfg.restore(cfgStat);
+                await this.wlanConfigurator.setIFaceCfg(devname, ifname, cfg);
+            }
         },
-        setIFaceMACAuthMode: async() => {
-            //
+        setIFaceMACAuthMode: async(devname: string, ifname: string, mode: string): Promise<void> => {
+            if (this.wlanConfigurator instanceof EFMWlanConfigurator) {
+                return await this.wlanConfigurator.setIFaceMACAuthMode(devname, ifname, mode as any);
+            } else {
+                throw new Error('This feature is not supported.');
+            }
         },
-        setIFaceMACAuthDevice: async() => {
-            //
+        setIFaceMACAuthDevice: async(devname: string, ifname: string, device: {macaddr: string, name: string}): Promise<void> => {
+            if (this.wlanConfigurator instanceof EFMWlanConfigurator) {
+                return await this.wlanConfigurator.setIFaceMACAuthDevice(devname, ifname, device);
+            } else {
+                throw new Error('This feature is not supported.');
+            }
         },
-        getIFaceMACAuthMode: async() => {
-            //
+        getIFaceMACAuthMode: async(devname: string, ifname: string): Promise<string> => {
+            if (this.wlanConfigurator instanceof EFMWlanConfigurator) {
+                return await this.wlanConfigurator.getIFaceMACAuthMode(devname, ifname);
+            } else {
+                throw new Error('This feature is not supported.');
+            }
         },
-        getIFaceMACAuthList: async() => {
-            //
+        getIFaceMACAuthList: async(devname: string, ifname: string): Promise<{[key: string]: string}[]> => {
+            if (this.wlanConfigurator instanceof EFMWlanConfigurator) {
+                return await this.wlanConfigurator.getIFaceMACAuthList(devname, ifname);
+            } else {
+                throw new Error('This feature is not supported.');
+            }
         },
-        removeIFaceMACAuthDevice: async() => {
-            //
+        removeIFaceMACAuthDevice: async(devname: string, ifname: string, macaddr: string): Promise<void> => {
+            if (this.wlanConfigurator instanceof EFMWlanConfigurator) {
+                return await this.wlanConfigurator.removeIFaceMACAuthDevice(devname, ifname, macaddr);
+            } else {
+                throw new Error('This feature is not supported.');
+            }
         },
     }
     
--- src/driver/generic/WLANDevConfiguration.ts
+++ src/driver/generic/WLANDevConfiguration.ts
@@ -1,3 +1,19 @@
+export interface WLANDevConfigurationState {
+    disabled: boolean,
+    channel: number | 'auto',
+    hwmode: 'a' | 'b' | 'g',
+    htmode: string,
+    chanbw: '5' | '10' | '20' | '40' | '80' | '80+80' | '160',
+    txpower: number | 'auto',
+    diversity: boolean,
+    rxantenna: number | 'auto',
+    txantenna: number | 'auto',
+    country: string,
+    country_ie: boolean,
+    beacon_int: number,
+    require_mode: 'none' | 'b' | 'g' | 'n' | 'ac' | 'ax',
+}
+
 export class WLANDevConfiguration {
     public disabled = false; // I/F disable
     public channel: number | 'auto' = 'auto'; // Channel
@@ -15,5 +31,39 @@ export class WLANDevConfiguration {
 
     constructor() {
         //
+    }
+
+    public serialize(): WLANDevConfigurationState {
+        return {
+            disabled: this.disabled,
+            channel: this.channel,
+            hwmode: this.hwmode,
+            htmode: this.htmode,
+            chanbw: this.chanbw,
+            txpower: this.txpower,
+            diversity: this.diversity,
+            rxantenna: this.rxantenna,
+            txantenna: this.txantenna,
+            country: this.country,
+            country_ie: this.country_ie,
+            beacon_int: this.beacon_int,
+            require_mode: this.require_mode,
+        }
+    }
+
+    public restore(state: WLANDevConfigurationState): void {
+        this.disabled = state.disabled;
+        this.channel = state.channel;
+        this.hwmode = state.hwmode;
+        this.htmode = state.htmode;
+        this.chanbw = state.chanbw;
+        this.txpower = state.txpower;
+        this.diversity = state.diversity;
+        this.rxantenna = state.rxantenna;
+        this.txantenna = state.txantenna;
+        this.country = state.country;
+        this.country_ie = state.country_ie;
+        this.beacon_int = state.beacon_int;
+        this.require_mode = state.require_mode;
     }
 }
