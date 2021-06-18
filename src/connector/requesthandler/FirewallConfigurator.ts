8195d26d1943a17b3eb41e0dc77af93ffd4e85f6
Yonghyu Ban <yonghyu@empo.im>
2021-06-18 17:25:25 +0900

    Implement FirewallConfigurator RPC handler

--- src/connector/requesthandler/FirewallConfigurator.ts
+++ src/connector/requesthandler/FirewallConfigurator.ts
@@ -1,10 +1,22 @@
 import { FirewallConfigurator as EFMFirewallConfigurator } from '../../driver/efm/FirewallConfigurator';
-import { FirewallConfigurator as GenericFirewallConfigurator } from '../../driver/generic/FirewallConfigurator';
+import { FirewallConfigurator as GenericFirewallConfigurator, FirewallEntry, DNATRule } from '../../driver/generic/FirewallConfigurator';
 import { RPCMethodTable, RPCRequestHandler } from './RPCRequestHandler';
 
 export class FirewallConfiguratorReqHandler extends RPCRequestHandler {
     private firewallConfigurator: GenericFirewallConfigurator | EFMFirewallConfigurator;
     protected rpcMethodTable: RPCMethodTable = {
+        getFirewallConfiguration: async(): Promise<FirewallEntry[]> => {
+            return await this.firewallConfigurator.getFirewallConfiguration();
+        },
+        setFireawllConfiguration: async(cfgs: FirewallEntry[]): Promise<void> => {
+            await this.firewallConfigurator.setFirewallConfiguration(cfgs);
+        },
+        getDNATRules: async(): Promise<DNATRule[]> => {
+            return await this.firewallConfigurator.getDNATRules();
+        },
+        setDNATRules: async(rules: DNATRule[]): Promise<void> => {
+            await this.firewallConfigurator.setDNATRules(rules);
+        },
     };
 
     constructor(deviceId: string, firewallConfigurator: GenericFirewallConfigurator | EFMFirewallConfigurator) {
--- src/driver/generic/FirewallConfigurator.ts
+++ src/driver/generic/FirewallConfigurator.ts
@@ -1,3 +1,29 @@
+export interface FirewallEntry {
+    /** rule name (human readable) */
+    name: string,
+    /** source zone */
+    src: string | undefined | null,
+    /** Source IP address. */
+    src_ip: string | undefined | null,
+    /** Source MAC address. */
+    src_mac: string | undefined | null,
+    /** Source port */
+    src_port: number | string | undefined | null,
+    /** Protocol */
+    proto: 'all' | 'tcp' | 'udp' | 'igmp', // Or whatever. Need to research more
+    icmp_type: string | undefined | null,
+    dest: string | undefined | null,
+    dest_ip: string | undefined | null,
+    dest_port: number | string | undefined | null,
+    ipset: string | undefined | null,
+    mark: number | undefined | null,
+    target: string | undefined | null,
+    set_mark: number | undefined | null,
+    set_xmark: number | undefined | null,
+    family: 'any' | 'ipv4' | 'ipv6',
+    enabled: boolean,
+}
+
 export interface DNATRule {
     name: string,
     src: string,
@@ -9,6 +35,8 @@ export interface DNATRule {
 }
 
 export abstract class FirewallConfigurator {
-    public abstract getDNATRules(): Promise<[DNATRule]>;
-    public abstract setDNATRules(rules: [DNATRule]): Promise<void>;
+    public abstract getFirewallConfiguration(): Promise<FirewallEntry[]>;
+    public abstract setFirewallConfiguration(cfgs: FirewallEntry[]): Promise<void>;
+    public abstract getDNATRules(): Promise<DNATRule[]>;
+    public abstract setDNATRules(rules: DNATRule[]): Promise<void>;
 }
