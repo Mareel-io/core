2efa26dbeae61b18527eb9472e58fbfd27149c12
Yonghyu Ban <yonghyu@empo.im>
2021-06-29 16:00:33 +0900

    implement test for getDevices() api

--- test-integration/rpc/RPCConnectionTest.ts
+++ test-integration/rpc/RPCConnectionTest.ts
@@ -1,3 +1,16 @@
+import WebSocket from "ws";
+import { RPCControllerFactory } from "../../dist";
+
+let rpcControllerFactory: RPCControllerFactory | null = null;
+
 describe('RPC daemon test', () => {
-    it('spawn test')
+    before(async () => {
+        const connection = new WebSocket('http://localhost:4000');
+        rpcControllerFactory = new RPCControllerFactory(connection);
+        await rpcControllerFactory.authenticate();
+    });
+
+    it('getDevices', async () => {
+        await rpcControllerFactory.getDevices();
+    });
 })
