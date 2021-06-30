1d85d4601353cdde69972e890efc97776b5dfd6b
Yonghyu Ban <yonghyu@empo.im>
2021-06-30 16:15:35 +0900

    Fix error class

--- src/error/MarilError.ts
+++ src/error/MarilError.ts
@@ -3,46 +3,39 @@ import { CustomError } from "ts-custom-error";
 export class MarilError extends CustomError {
     constructor(msg: string) {
         super(msg);
-        this.name = 'MarilError';
     }
 }
 
 export class MarilRPCError extends MarilError {
     constructor(msg: string, name = 'MarilRPCError') {
         super(msg);
-        this.name = name;
     }
 }
 
 export class InvalidParameterError extends MarilError {
     constructor(msg: string) {
         super(msg);
-        this.name = 'InvalidParameterError';
     }
 }
 
 export class AuthError extends MarilError {
     constructor(msg: string) {
         super(msg);
-        this.name = 'AuthError';
     }
 }
 export class UnsupportedFeatureError extends MarilError {
     constructor(msg: string) {
         super(msg);
-        this.name = 'UnsupportedFeatureError';
     }
 }
 export class ResourceNotAvailableError extends MarilError {
     constructor(msg: string) {
         super(msg);
-        this.name = 'ResourceNotAvailableError';
     }
 }
 
 export class MethodNotImplementedError extends MarilError {
     constructor(msg?: string) {
         super(msg || 'Method not implemented.');
-        this.name = 'MethodNotImplementedError';
     }
 }
