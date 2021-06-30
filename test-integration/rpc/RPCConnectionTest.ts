34a2071d8cc702957ce6cf5b62db547fce1f2168
Yonghyu Ban <yonghyu@empo.im>
2021-06-30 14:42:17 +0900

    Implement basic pinging test

--- test-integration/rpc/RPCConnectionTest.ts
+++ test-integration/rpc/RPCConnectionTest.ts
@@ -1,8 +1,17 @@
+import * as chai from "chai";
 import { doRPCTest, RPCConnectionHelperCtx } from '../../test-util/RPCConnectionHelper';
 
 doRPCTest('RPC daemon test', (ctx: RPCConnectionHelperCtx) => {
     it('getDevices', async () => {
         console.log('getDevices');
         ctx.rpcControllerFactory.getDevices();
+    });
+
+    it('ping', async() => {
+        await ctx.rpcControllerFactory.ping().should.eventually.fulfilled;
+    });
+
+    it('error', async() => {
+        await ctx.rpcControllerFactory.errorPing().should.eventually.rejected;
     });
 });
