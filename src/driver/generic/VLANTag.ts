15a2a373fcc8c2843ae3b196ba5a1a7a4899895b
Yonghyu Ban <yonghyu@empo.im>
2021-08-24 13:57:58 +0900

    TSDoc for GenericVLANTag

--- src/driver/generic/VLANTag.ts
+++ src/driver/generic/VLANTag.ts
@@ -10,6 +10,7 @@ export class VLANTag {
     /**
      * Convert its state into normal k-v pair form.
      * Do not fiddle with this. Not guaranteed to be stable
+     *
      * @returns Current state. Do not fiddle with this
      */
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -20,21 +21,41 @@ export class VLANTag {
         }
     }
 
+    /**
+     * Restore its state from k-v pair from. Intended for internal use.
+     *
+     * @param restoreData State snapshotted using serialize()
+     */
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     public restore(restoreData: {[key: string]: any}) {
         this.tag = restoreData.tag;
         this.alias = restoreData.alias;
     }
 
+    /**
+     * Returns VLAN tag number
+     *
+     * @returns VLAN tag number
+     */
     public getTag(): number {
         return this.tag;
     }
 
+    /**
+     * Returns VLAN alias name
+     * 
+     * @returns VLAN alias
+     */
     public getAlias(): string {
         return this.alias;
     }
 
-    setAlias(alias: string) {
+    /**
+     * Set VLAN alias name
+     * 
+     * @param alias 
+     */
+    public setAlias(alias: string) {
         this.alias = alias;
     }
 }
