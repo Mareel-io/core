1bcd33d2cd0af5975070afacfda27fa23475fcef
Yonghyu Ban <yonghyu@empo.im>
2021-08-24 11:47:43 +0900

    Partial documentation of WLAN API

--- src/connector/classwrapper/WLANConfigurator.ts
+++ src/connector/classwrapper/WLANConfigurator.ts
@@ -14,6 +14,11 @@ export class RPCWLANConfigurator extends WLANConfigurator {
         this.targetId = targetId;
     }
 
+    /**
+     * Get WLAN device list
+     * 
+     * @returns WLAN device name list
+     */
     public async getDeviceList(): Promise<string[]> {
         return (await this.rpc.remoteCall({
             jsonrpc: '2.0',
@@ -24,6 +29,12 @@ export class RPCWLANConfigurator extends WLANConfigurator {
         })) as string[];
     }
 
+    /**
+     * Get WLAN device configuration
+     * 
+     * @param devname WLAN device name
+     * @returns WLAN device configuration
+     */
     public async getDeviceCfg(devname: string): Promise<WLANDevConfiguration> {
         const res = (await this.rpc.remoteCall({
             jsonrpc: '2.0',
@@ -38,6 +49,12 @@ export class RPCWLANConfigurator extends WLANConfigurator {
         return wldev;
     }
 
+    /**
+     * Apply WLAN device configuration to specific WLAN device
+     * 
+     * @param devname WLAN device name
+     * @param cfg WLAN device configuration
+     */
     public async setDeviceCfg(devname: string, cfg: WLANDevConfiguration): Promise<void> {
         await this.rpc.remoteCall({
             jsonrpc: '2.0',
@@ -48,6 +65,13 @@ export class RPCWLANConfigurator extends WLANConfigurator {
         });
     }
 
+    /**
+     * Get specific interface configuration using wlan devname and ifname
+     * 
+     * @param devname WLAN device which holds given interface
+     * @param ifname WLAN interface which belongs to given WLAN device
+     * @returns WLAN interface configuration
+     */
     public async getIFaceCfg(devname: string, ifname: string): Promise<WLANIFaceCfg> {
         const res = (await this.rpc.remoteCall({
             jsonrpc: '2.0',
@@ -62,6 +86,13 @@ export class RPCWLANConfigurator extends WLANConfigurator {
         return wlifcfg;
     }
 
+    /**
+     * Apply given config to given WLAN interface.
+     * 
+     * @param devname WLAN device which holds given interface
+     * @param ifname WLAN interface which belongs to given WLAN device
+     * @param cfg WLAN interface configuration
+     */
     public async setIFaceCfg(devname: string, ifname: string, cfg: WLANIFaceCfg): Promise<void> {
         await this.rpc.remoteCall({
             jsonrpc: '2.0',
