311579899734e1261d0157ffc9b5eb49c1456976
Yonghyu Ban <yonghyu@empo.im>
2021-10-14 12:46:04 +0900

    Generic driver for Traffic status monitor

--- src/driver/fortinet/monitor/TrafficStat.ts
+++ src/driver/fortinet/monitor/TrafficStat.ts
@@ -1,15 +1,17 @@
 import { AxiosInstance } from "axios";
-import { FortiResponse } from "../util/types";
+import { TrafficStat, TrafficStatMonitor } from "../../generic/monitor/TrafficStatMonitor";
+import { FortiResponse, FortiTrafficStat } from "../util/types";
 
-export class FortiTrafficStat 
+export class FortiTrafficStatMonitor extends TrafficStatMonitor
 {
     private api: AxiosInstance;
 
     constructor(api: AxiosInstance) {
+        super();
         this.api = api;
     }
 
-    public async getTrafficStat(destzone: string) {
+    public async getTrafficStat(): Promise<TrafficStat[]> {
         const result = await this.api.get('/api/v2/monitor/fortiview/statistics', {
             params: {
                 vdom: 'root',
@@ -24,5 +26,23 @@ export class FortiTrafficStat
         });
 
         const resp: FortiResponse<FortiTrafficStat> = result.data;
+
+        return resp.results.details.map((elem) => {
+            return {
+                srcip: elem.srcaddr,
+                dstip: elem.dstaddr,
+                total_rx_pkt: elem.rx_packets,
+                total_tx_pkt: elem.tx_packets,
+                total_rx_bw: elem.rx_bandwidth,
+                total_tx_bw: elem.tx_bandwidth,
+                apps: elem.apps.map((app) => {
+                    return {
+                        count: app.count,
+                        proto: app.protocol as unknown as 'TCP' | 'UDP',
+                        port: app.port,
+                    }
+                }),
+            }
+        })
     }
 }
