889f5cbaa69d17a4a3edfc1da4edc0678a982f0e
Yonghyu Ban <yonghyu@empo.im>
2021-11-01 10:33:00 +0900

    Fix mistake

--- src/cli/connectord.ts
+++ src/cli/connectord.ts
@@ -160,7 +160,7 @@ export class ConnectorClient {
 
     private handleDriverInitError(device: string, feature: string, e: MethodNotImplementedError | Error): void {
         if (e instanceof MethodNotImplementedError) {
-            logger.warn(`Device ${device} feature: ${feature} is work in progress.`);
+            logger.warning(`Device ${device} feature: ${feature} is work in progress.`);
         } if (e instanceof UnsupportedFeatureError) {
             logger.info(`Device ${device} does not support feature: ${feature}`);
         } else {
