c722f90ee8ba5026c3851de1d35816186c626426
Yonghyu Ban <yonghyu@empo.im>
2021-11-16 12:53:23 +0900

    Generic VPN configuration data type

--- src/driver/generic/VPNConfigurator.ts
+++ src/driver/generic/VPNConfigurator.ts
@@ -1,6 +1,20 @@
+export interface SSLVPN {
+    ssl?: {
+        maxProto: string,
+        milProto: string,
+        allowedCipher?: string,
+        bannedCipher?: string,
+    },
+    port: number,
+}
+
+export interface IPSECVPN {
+    // TODO: Implement me.
+}
+
 export abstract class VPNConfigurator {
     public abstract getVPNConfigurations(type: string): Promise<any[]>;
-    public abstract setVPNConfiguration(type: string, idx: number, config: any): Promise<void>;
+    public abstract setVPNConfiguration(type: string, idx: number, config: SSLVPN | IPSECVPN): Promise<void>;
     public abstract deleteVPNConfiguration(type: string, idx: number): Promise<void>;
-    public abstract addVPNConfiguration(type: string, config: any): Promise<void>;
+    public abstract addVPNConfiguration(type: string, config: SSLVPN | IPSECVPN): Promise<void>;
 }
